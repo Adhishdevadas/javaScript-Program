e0bad6601715ca0a5baea33fceda7120cd31c477
Adhishdevadasan <144409643+Adhishdevadas@users.noreply.github.com>
2024-07-05 21:29:50 +0530

    Add files via upload

---
+++ 02_js_variable/ch_prg_04.js
@@ -0,0 +1,13 @@
+var score;
+score = 100;
+console.log(score);
+
+score = 150;
+console.log(score);
+
+score2 = 50;
+console.log(score2); 
+
+score2 = 75;
+
+console.log(score2);
---
+++ 02_js_variable/ch_prg_09.js
@@ -0,0 +1,22 @@
+var thisIsFine;
+var $noProblemHere;
+var _underscore56;
+var StartWithCapital;
+var z5;
+
+
+var Steps;  
+var nope;  
+var single;  
+var yield; 
+
+
+
+
+var thisIsFine;
+var $noProblemHere;
+var _underscore56;
+var StartWithCapital;
+var z5;
+
+
